e4347c05ffd9d40d2dcf3756076119d3cf3452f8
AniketPatil80 <128156963+AniketPatil80@users.noreply.github.com>
2023-08-23 01:22:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AboutUs from "./Pages/AboutUs";
+import ContactUs from "./Pages/ContactUs";
+
+function App() {
+  return (
+    <>
+      {/* <AboutUs /> */}
+      <ContactUs />
+    </>
+  );
+}
+
+export default App;
